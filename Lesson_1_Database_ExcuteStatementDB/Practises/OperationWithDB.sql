b15b3cb6f0afd00f163e754bc5c48ab2527fd6e8
sangle173 <leducsang.10dt2@gmail.com>
2021-03-25 23:49:22 +0700

    upload lesson 1

---
+++ Lesson_1_Database_ExcuteStatementDB/Practises/OperationWithDB.sql
@@ -0,0 +1,8 @@
+SELECT * FROM classicmodels.customers
+where customerName like '%Toys%';
+SELECT * FROM classicmodels.customers
+where customerNumber>=200;
+SELECT * FROM classicmodels.customers
+where phone like '%2555%';
+SELECT * FROM classicmodels.customers
+where customerName= 'Atelier graphique';
