2b2ac9ddc6afadbabc29aa20a494cbee549bb0fd
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-04-02 17:13:40 +0700

    update

--- Database-CaseStudy/furama_resort_case_study.sql
+++ Database-CaseStudy/furama_resort_case_study.sql
@@ -1,4 +1,3 @@
-drop database if exists furama_resort_dababase;
 create database furama_resort_dababase;
 
 use furama_resort_dababase;
@@ -179,27 +178,29 @@ values
     
 insert into Employees( full_name, position_id, employee_level_id, department_id, date_of_birth, id_card_number, salary, phone_number, email, address) 
 value 
-	('Le Duc Sang', 1, 2, 3, '1992-06-19', 208876544, 12000000.00, 0333576574, 'sangle@gmail.com', '103 Hoang Dieu' ),
-	('Nguyen Duc Hau', 2, 1, 4, '1996-07-25', 208876545, 16000000.00, 090765654, 'hau_nguyen@gmail.com', 'Hoa Tien' ),
-	('Bui Thanh Tai', 1, 1, 1, '1996-10-17', 207776513, 10000000.00, 091767658, 'tai_bui@gmail.com', 'Hue' ),
-	('Nguyen Thanh Tung', 1, 2, 3, '1988-07-23', 209971245, 18000000.00, 0333765654, 'tung_nguyen@gmail.com', 'Da Nang' ),
-	('Ho Quang Kha', 2, 2, 2, '1995-08-30', 208877619, 15000000.00, 0345717654, 'kha_ho@gmail.com', 'Binh Dinh' ),
-	('Nguyen Trung Kien', 1, 3, 2, '1999-09-30', 208845545, 10000000.00, 090765333, 'kien_nguyen@gmail.com', 'Da Nang' ),
-	('Ngueyn Huy', 1, 3, 2, '1987-07-29', 212376545, 16000000.00, 094445654, 'huy_nguyen@gmail.com', 'Da Nang' ),
-	('Dinh Van Thang', 2, 3, 1, '1992-10-17', 208856453, 100000000.00, 0333765654, 'thang_dinh@gmail.com', 'Quang Nam' ),
-	('Nguyen Van Duc', 1, 3, 4, '1998-07-30', 218846565, 8000000.00, 091763674, 'duc_nguyen@gmail.com', 'Nghe An' );
+	('Le Duc Sang', 1, 2, 3, '1992-06-19', '208876544', 12000000.00, '0333576574', 'sangle@gmail.com', '103 Hoang Dieu' ),
+	('Nguyen Duc Hau', 2, 1, 4, '1996-07-25', '208876545', 16000000.00, '0907656547', 'hau_nguyen@gmail.com', 'Hoa Tien' ),
+	('Bui Thanh Tai', 1, 1, 1, '1996-10-17', '207776513', 10000000.00, '0917676587', 'tai_bui@gmail.com', 'Hue' ),
+	('Nguyen Thanh Tung', 1, 2, 3, '1988-07-23', '209971245', 18000000.00, '0333765654', 'tung_nguyen@gmail.com', 'Da Nang' ),
+	('Ho Quang Kha', 2, 2, 2, '1995-08-30', '208877619', 15000000.00, '0345717654', 'kha_ho@gmail.com', 'Binh Dinh' ),
+	('Nguyen Trung Kien', 1, 3, 2, '1999-09-30', '208845545', 10000000.00, '0907658333', 'kien_nguyen@gmail.com', 'Da Nang' ),
+	('Ngueyn Huy', 1, 3, 2, '1987-07-29', '212376545', 16000000.00, '0944458654', 'huy_nguyen@gmail.com', 'Da Nang' ),
+	('Dinh Van Thang', 2, 3, 1, '1992-10-17', '208856453', 100000000.00, '0333765654', 'thang_dinh@gmail.com', 'Quang Nam' ),
+	('Nguyen Van Duc', 1, 3, 4, '1998-07-30', '218846565', 8000000.00, '0917636784', 'duc_nguyen@gmail.com', 'Nghe An' );
+select length(phone_number) 
+from employees;
 
  insert into Customers(customer_type_id, full_name, date_of_birth, id_card_number, phone_number, email ,address)
  value
-	(1, 'Joan', '1977-06-20', 208872125, 0333576575, 'joan@gmail.com', 'Spanish' ),
-	(2, 'Take Uchi Satoru', '1971-01-19', 208876546, 090765659, 'take_uchi@gmail.com', 'Japan' ),
-	(2, 'Ty Colin', '1975-11-28', 207776517, 091767658, 'ty_colin@gmail.com', 'Us' ),
-	(1, 'Nguyen Minh Phuc', '1984-08-19', 209971242, 0333765651, 'phuc_nguyen@gmail.com', 'Ha Noi' ),
-	(2, 'Joan', '1920-11-19', 208877611, 03457176510, 'ha_nguyen@gmail.com', 'Quang Tri' ),
-	(3, 'Nguyen Hong Dang', '1988-12-30', 208845540, 090765335, 'dang_nguyen@gmail.com', 'Ha Noi' ),
-	(2, 'Lam Ba Dat', '1987-07-29', 212376541, 094445656, 'ba_dat@gmail.com', 'Sai Gon' ),
-	(1, 'Cao Van Minh', '1992-10-17', 208856459, 0333765651, 'minh_cao@gmail.com', 'Cao Bang' ),
-	(2, 'Nguyen Dang Khoi', '1998-08-24', 218846561, 091763678, 'khoi_nguyen@gmail.com', 'Da Nang' );
+	(1, 'Joan', '1977-06-20', '208872125', '0333576575', 'joan@gmail.com', 'Spanish' ),
+	(2, 'Take Uchi Satoru', '1971-01-19', '208876546', '090765659', 'take_uchi@gmail.com', 'Japan' ),
+	(2, 'Ty Colin', '1975-11-28', '207776517', '091767658', 'ty_colin@gmail.com', 'Us' ),
+	(1, 'Nguyen Minh Phuc', '1984-08-19', '209971242', '0333765651', 'phuc_nguyen@gmail.com', 'Ha Noi' ),
+	(2, 'Joan', '1920-11-19', '208877611', '0345176510', 'ha_nguyen@gmail.com', 'Quang Tri' ),
+	(3, 'Nguyen Hong Dang', '1988-12-30', '208845540', '0906588335', 'dang_nguyen@gmail.com', 'Ha Noi' ),
+	(2, 'Lam Ba Dat', '1987-07-29', '212376541', '0944456056', 'ba_dat@gmail.com', 'Sai Gon' ),
+	(1, 'Cao Van Minh', '1992-10-17', '208856459','0333765651', 'minh_cao@gmail.com', 'Cao Bang' ),
+	(2, 'Nguyen Dang Khoi', '1998-08-24', '218846561', '0917063678', 'khoi_nguyen@gmail.com', 'Da Nang' );
     
 insert into Contracts(employee_id, customer_id, service_id, date_started, date_finished, down_payment, total_payment)
 value
@@ -215,7 +216,9 @@ value
 	(7, 7, 6, '2020-10-16', '2020-10-28', 150000, 1000000),
 	(7, 8, 5, '2021-03-16', '2021-03-20', 300000, 1000000);
     
-    
+insert into Contracts(employee_id, customer_id, service_id, date_started, date_finished, down_payment, total_payment)
+value
+    (7, 6, 2, '2019-02-07', '2019-11-19', 150000, 1000000);
  insert into ContractDetail( contract_id, accompanied_service_id, amount)
  value
 	(1, 1, 1 ),
@@ -225,6 +228,15 @@ value
 	(5, 5, 3 ),
 	(7, 2, 1 ),
 	(8, 3, 1 );
+insert into ContractDetail( contract_id, accompanied_service_id, amount)
+ value
+	(10, 1, 1 ),
+	(10, 2, 2 ),
+	(10, 3, 3 ),
+	(11, 4, 2 ),
+	(11, 5, 3 ),
+	(9, 2, 1 ),
+	(12, 3, 1 );
 -- Task 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.
 select *
 from employees
@@ -235,6 +247,22 @@ where (full_name regexp '[HKT][a-z]*$') and length(full_name) <=15;
 select *
 from customers
 where (timestampdiff(year, date_of_birth, curdate()) between 18 and 50 ) and address in ('Da Nang', 'Quang Tri'); 
+-- tính tuổi
+select full_name, ROUND(DATEDIFF(CURDATE(), date_of_birth) / 365, 0) AS age
+from customers;
+
+select full_name,(YEAR(CURDATE()) - YEAR(date_of_birth)) - (RIGHT(CURDATE(), 5) < RIGHT(date_of_birth, 5)) AS age
+from customers;
+
+-- tính ngày sinh nhật là thứ mấy
+
+select full_name, dayname(date_of_birth)
+from customers;
+
+-- tính tháng sinh nhật
+
+select full_name, monthname(date_of_birth)
+from customers;
 
 -- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được 
 -- sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
@@ -300,17 +328,153 @@ group by full_name;
 
 -- third way
 
+select *
+from (
+	select *, row_number() over (partition by full_name order by customer_id) as rn from customers 
+) t
+where rn=2
+order by customer_id;
+
+-- fourth way
+
+select *
+from customers t1
+	left join customers t2 on t1.full_name = t2.full_name and t1.customer_id>t2.customer_id
+where t2.full_name is null;
+
+-- fifth way
+ select *
+ from customers t1
+where not exists (
+	select 1
+    from customers t2
+    where t1.full_name= t2.full_name and t1.customer_id > t2.customer_id
+)
+order by customer_id;
+
 -- 9. Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2019 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+insert into Contracts(employee_id, customer_id, service_id, date_started, date_finished, down_payment, total_payment)
+value
+	(7, 1, 2, '2019-11-01', '2019-11-20', 150000, 1600000),
+	(2, 2, 2, '2019-12-08', '2019-12-28', 100000, 3000000);
+    
+select month(contracts.date_started)as month_booking, sum(total_payment) as total_payment_in_month, count(*) as no_of_booking
+from contracts
+where year(contracts.date_started)=2019
+group by month(contracts.date_started)
+order by month(contracts.date_started);
 
-
-
-
-
--- 10. Hiển thị thông tin tương ứng với từng Hợp đồng thì đã sử dụng bao nhiêu Dịch vụ đi kèm. Kết quả hiển thị bao gồm IDHopDong, NgayLamHopDong, NgayKetthuc, TienDatCoc,
+-- 10. Hiển thị thông tin tương ứng với từng Hợp đồng thì đã sử dụng bao nhiêu Dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm IDHopDong, NgayLamHopDong, NgayKetthuc, TienDatCoc,
 -- SoLuongDichVuDiKem (được tính dựa trên việc count các IDHopDongChiTiet).
--- 11. Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
--- 12. Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 
--- tháng cuối năm 2019 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019.
--- 13. Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
--- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
 
+select contracts.contract_id, contracts.date_started, contracts.date_finished, contracts.down_payment, contractdetail.amount, count(*) as noOfAccompaniedService
+from contracts left join contractdetail on contractdetail.contract_id = contracts.contract_id
+group by contract_id
+order by noOfAccompaniedService;
+
+-- 11. Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có 
+-- TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
+
+select customers.customer_id, customers.address, customertype.customer_type_name, accompaniedservice.accompanied_service_name, accompaniedservice.price
+from customers 
+	inner join customertype on customers.customer_type_id=customertype.customer_type_id
+	left join contracts on contracts.customer_id =customers.customer_id
+    inner join contractdetail on contractdetail.contract_id=contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id=contractdetail.accompanied_service_id
+where customertype.customer_type_name='Diamond' and customers.address in ('Quang Tri', 'Ha Noi');
+
+-- 12. Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem 
+-- (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 
+-- tháng cuối năm 2019 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019.
+
+select contracts.contract_id, employees.full_name as employeeName, customers.full_name as customerName, customers.phone_number as customerPhoneNumber, services.service_name
+, count(*) as amountOfAccompaningSerivce, contracts.date_started, contracts.down_payment
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+where (month(date_started) between 9 and 12) and (year(date_started)=2019) and month(date_started) not in (
+	select contract_id
+    from contracts
+    where month(date_started) between 1 and 6
+)
+group by contracts.contract_id
+order by amountOfAccompaningSerivce;
+
+-- 13. Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+--  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+set @max_of_use= (select max(maxOfAc) 
+from (select accompaniedservice.accompanied_service_id, count(accompaniedservice.accompanied_service_id)as maxOfAc
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id= contractdetail.accompanied_service_id
+group by accompaniedservice.accompanied_service_id) as nou);
+
+select  @max_of_use;
+
+set @min_of_use = (select min(minOfAc) 
+from (select accompaniedservice.accompanied_service_id, count(accompaniedservice.accompanied_service_id)as minOfAc
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id= contractdetail.accompanied_service_id
+group by accompaniedservice.accompanied_service_id) as nou);
+
+select @min_of_use;
+-- select @max_of_use;
+-- SELECT *
+-- FROM extra_services
+-- WHERE extra_service_id IN (SELECT
+-- 	extra_service_id FROM (SELECT
+-- 	es.extra_service_id, COUNT(es.extra_service_id) AS number_of_use
+-- 	FROM employees e
+-- 	INNER JOIN agreements a ON a.employee_id = e.employee_id
+--     INNER JOIN customers c ON c.customer_id = a.customer_id
+--     INNER JOIN services s ON s.service_id= a.service_id
+--     INNER JOIN detailed_agreements da ON da.agreement_id = a.agreement_id
+--     INNER JOIN extra_services es ON es.extra_service_id= da.extra_service_id
+-- 	GROUP BY es.extra_service_id
+-- 	) as nou
+--     where nou.number_of_use = @max_of_use
+-- );
+
+select contracts.contract_id, accompaniedservice.accompanied_service_name, count(accompaniedservice.accompanied_service_id)as noOfUsing
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id= contractdetail.accompanied_service_id
+group by accompaniedservice.accompanied_service_id
+having noOfUsing= @max_of_use
+order by noOfUsing;
+
+select contracts.contract_id, accompaniedservice.accompanied_service_name, count(accompaniedservice.accompanied_service_id)as noOfUsing
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id= contractdetail.accompanied_service_id
+group by accompaniedservice.accompanied_service_id
+having noOfUsing= @min_of_use
+order by noOfUsing;
+
+-- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
+select contracts.contract_id, accompaniedservice.accompanied_service_name, count(accompaniedservice.accompanied_service_id)as noOfUsing
+from employees
+	inner join contracts on contracts.employee_id=employees.employee_id
+    inner join customers on customers.customer_id=contracts.customer_id
+    inner join services on services.service_id= contracts.service_id
+    inner join contractdetail on contractdetail.contract_id= contracts.contract_id
+    inner join accompaniedservice on accompaniedservice.accompanied_service_id= contractdetail.accompanied_service_id
+group by accompaniedservice.accompanied_service_id
+having noOfUsing=1
+order by noOfUsing;
